8f9a859b0ae3b9284040e6a158b78d5a170645d5
Pablo Gallegos <pablogallegos102@gmail.com>
2026-06-28 14:44:34 -0300

    Agrege la funcion en javascript de cambiar el tema a modo oscuro. En programas, luego pregunto a mi grupo si ponerlo en cada archivo de los otros.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
  
+//Objetivo de esta funcion: al dar clik en boton que dice "Cambiar tema", toda la pagina pasa a modo oscuro. 
+const botonModoOscuro = document.getElementById("botonModoOscuro");
+
+botonModoOscuro.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+});
